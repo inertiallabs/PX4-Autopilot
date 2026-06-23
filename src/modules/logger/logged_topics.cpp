5b8937139266528360b0396b712e3e5d8e6931e0
Valentin Bugrov <vladbvnsk@gmail.com>
2026-06-22 21:40:28 -0400

    feat(module/logger): Add InertialLabs uorb-topic logging

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -157,6 +157,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic("fixed_wing_lateral_guidance_status", 100);
 	add_optional_topic("fixed_wing_lateral_status", 100);
 	add_optional_topic("fixed_wing_runway_control", 100);
+	add_topic("inertial_labs_ins", 50);
 
 	// multi topics
 	add_optional_topic_multi("actuator_outputs", 100, 3);
